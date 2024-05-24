ea88e405dc4313f5c2b36030ec25280a0582ac3c
navneet23341 <163036297+navneet23341@users.noreply.github.com>
2024-05-25 02:12:10 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,17 +7,14 @@ const thankYouMessage = document.getElementById('thank-you-message');
 let clickCount = 0;
 
 noBtn.addEventListener('click', (event) => {
-    event.stopPropagation(); // Prevent the click event from propagating to the "Yes" button
+    event.stopPropagation();
     clickCount++;
     yesBtn.style.transform = `scale(${1 + clickCount * 0.5})`;
 });
 
 yesBtn.addEventListener('click', () => {
-    // Hide the buttons
     yesBtn.style.display = 'none';
     noBtn.style.display = 'none';
-
-    // Display the "Thank You" message and image container
     thankYouMessage.classList.add('visible');
     imageContainer.classList.add('visible');
 });
